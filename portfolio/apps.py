96ff4acff7bf14f7ae4b3389235fd2c71b3685e0
DpiTStudio <admin@dpitstudio.ru>
2026-01-01 01:19:28 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   mysite/__pycache__/settings.cpython-314.pyc
    	modified:   portfolio/__pycache__/apps.cpython-314.pyc
    	modified:   portfolio/apps.py

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class Portfolio(AppConfig):
+class PortfolioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "portfolio"
     verbose_name = "Портфолио"
