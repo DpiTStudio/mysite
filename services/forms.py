617cdc7bbc100615d4eb96837ba4d77859a20de0
DpiTStudio <admin@dpitstudio.ru>
2026-01-07 04:07:47 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   db.sqlite3
    	modified:   services/forms.py

--- services/forms.py
+++ services/forms.py
@@ -13,6 +13,21 @@ class ServiceAdminForm(forms.ModelForm):
     class Meta:
         model = Service
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'style': 'width: 600px;'}),
+            'slug': forms.TextInput(attrs={'style': 'width: 400px;'}),
+            'short_description': forms.Textarea(attrs={'rows': 3, 'style': 'width: 800px;'}),
+            'description': forms.Textarea(attrs={'rows': 10, 'style': 'width: 800px;'}),
+            'technical_requirements': forms.Textarea(attrs={'rows': 6, 'style': 'width: 800px;'}),
+            'meta_description': forms.Textarea(attrs={'rows': 3, 'style': 'width: 800px;'}),
+            'meta_keywords': forms.Textarea(attrs={'rows': 3, 'style': 'width: 800px;'}),
+            'icon': forms.FileInput(attrs={'style': 'width: 400px;'}),
+            'image': forms.FileInput(attrs={'style': 'width: 400px;'}),
+            'logo': forms.FileInput(attrs={'style': 'width: 400px;'}),
+            'price_min': forms.NumberInput(attrs={'style': 'width: 100px;'}),
+            'price_max': forms.NumberInput(attrs={'style': 'width: 100px;'}),
+            'price': forms.NumberInput(attrs={'style': 'width: 100px;'}),
+        }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
