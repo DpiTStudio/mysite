6ebeceb97b1431a8f6a1ea602ebc8d13b7518f48
DpiTStudio <admin@dpitstudio.ru>
2026-02-22 20:33:03 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   context_processors.py
    	modified:   db.sqlite3
    	new file:   mysite/context_processors.py
    	modified:   mysite/settings.py
    	new file:   static/css/theme_seasons.css
    	new file:   staticfiles/css/theme_seasons.b9d463a8a8d6.css
    	new file:   staticfiles/css/theme_seasons.b9d463a8a8d6.css.gz
    	new file:   staticfiles/css/theme_seasons.css
    	new file:   staticfiles/css/theme_seasons.css.gz
    	modified:   staticfiles/staticfiles.json
    	modified:   templates/base.html

---
+++ context_processors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Контекстный процессор для определения текущей темы сайта в зависимости от сезона."""
+import datetime
+
+def season_theme(request):
+    """Возвращает название текущего сезона (spring, summer, autumn, winter) на основе месяца.
+    Это значение будет доступно в шаблонах как `current_season`.
+    """
+    month = datetime.datetime.now().month
+    if month in (12, 1, 2):
+        season = "winter"
+    elif month in (3, 4, 5):
+        season = "spring"
+    elif month in (6, 7, 8):
+        season = "summer"
+    else:
+        season = "autumn"
+    return {"current_season": season}
