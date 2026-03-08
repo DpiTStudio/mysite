194e9325a4500f66e556bc78ecf7deccf352927f
DpiTStudio <admin@dpitstudio.ru>
2026-03-08 21:28:23 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   cart/apps.py
    	modified:   db.sqlite3
    	modified:   mysite/settings.py
    	modified:   portfolio/templates/portfolio/list copy.html
    	new file:   static/css/portfolio_list.css
    	modified:   static/css/style.css
    	modified:   templates/category-bl.html
    	modified:   templates/header.html
    	modified:   templates/includes/footer_logo_inform.html

--- cart/apps.py
+++ cart/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class CartConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
+    verbose_name = 'Корзина'
+    verbose_name_plural = 'Корзины'
+    icon = 'fas fa-shopping-cart'
+    order = 10
+    
+    menu = {
+        'title': 'Корзина',
+        'icon': 'fas fa-shopping-cart',
+        'order': 10,
+    }
